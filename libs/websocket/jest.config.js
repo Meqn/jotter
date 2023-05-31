142f9d2e75738ce42f33dc3ca810ce85479b8f6a
Mervin <mengqing723@gmail.com>
2023-05-31 20:19:28 +0800

    ci(websocket): 更新打包配置

--- libs/websocket/jest.config.js
+++ libs/websocket/jest.config.js
@@ -1,11 +1,1 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  // roots: ['<rootDir>'],
-  // preset: 'ts-jest',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testEnvironment: 'node'
-}
+module.exports = require('../../jest.config')
