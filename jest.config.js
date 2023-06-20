8de77a08bcaa9d7c400804a243922f4c824c7b64
Mervin <mengqing723@gmail.com>
2023-06-20 22:44:09 +0800

    ci: update jest.config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,10 @@ module.exports = {
   // roots: ['<rootDir>'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverage: true, // 是否显示覆盖率报告
 }
